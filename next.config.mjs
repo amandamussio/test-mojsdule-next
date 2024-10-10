57f9d8d2408d7e105a9123cd68999bb23d78f762
Amanda Macedo Mussio <mussioamanda@gmail.com>
2024-10-10 09:27:26 -0300

    add button

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    assetPrefix: assetHost ? assetHost : undefined,
+    async headers() {
+        return [
+            {
+                source: '/:all*(.js|.css)',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=31536000, must-revalidate',
+                    },
+                ],
+            },
+        ];
+    },
+};
 
 export default nextConfig;
